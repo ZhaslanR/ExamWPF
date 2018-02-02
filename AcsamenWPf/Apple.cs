0ad844fe9557d2b7763526e2f2378efc6b97eb8e
ZhaslanR <zhaslan_12@bk.ru>
2018-02-02 17:52:23 +0600

    Добавьте файлы проекта.

---
+++ AcsamenWPf/Apple.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcsamenWPf
+{
+    public class Apple
+    {
+        private int profit;
+        private int price;
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+                ValuesChanged("Price");
+            }
+        }
+        private int deliveryPrise;
+        public int DeliveryPrise
+        {
+            get
+            {
+                return deliveryPrise;
+            }
+            set
+            {
+                deliveryPrise = value;
+            }
+        }
+
+        public string City { get; set; }
+        public int Profit
+        {
+            get
+            {
+                return profit;
+            }
+            set
+            {
+                profit = value;
+                OnPropertyChanged("Profit");
+            }
+        }
+
+        public event Action SomethingChanged;
+        private void ValuesChanged(string propertyName)
+        {
+            Profit = price - DeliveryPrise;
+            OnPropertyChanged(propertyName);
+            SomethingChanged?.Invoke();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ AcsamenWPf/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcsamenWPf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Apple> apples;
+        private string path = "data.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FileRead();
+
+            if (apples.Count == 0)
+            {
+                apples.Add(new Apple() { City = "Астана", DeliveryPrise = 20 });
+                apples.Add(new Apple() { City = "Алмата", DeliveryPrise = 40 });
+                apples.Add(new Apple() { City = "Костанай", DeliveryPrise = 60 });
+            }
+            dataGrid.ItemsSource = apples;
+
+            FileWrite();
+        }
+
+        private void FileRead()
+        {
+            using (FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (StreamReader stream = new StreamReader(fileStream))
+                {
+                    using (JsonTextReader reader = new JsonTextReader(stream))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        apples = serializer.Deserialize<ObservableCollection<Apple>>(reader);
+                        if (apples == null) apples = new ObservableCollection<Apple>();
+                    }
+                }
+            }
+        }
+        private void FileWrite()
+        {
+            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter stream = new StreamWriter(fileStream))
+                {
+                    using (JsonTextWriter writer = new JsonTextWriter(stream))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        serializer.Serialize(writer, apples);
+                    }
+                }
+            }
+        }
+    }
+}
